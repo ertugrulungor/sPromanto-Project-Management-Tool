0dd22be8093a850e7f5bccf425923c17b28f46df
ertugrulungor <ertugrulungor@gmail.com>
2016-12-01 19:09:57 +0200

    Tasarımların bir kısmı aktarıldı

---
+++ YPYA/Controllers/SignController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace YPYA.Controllers
+{
+    public class SignController : Controller
+    {
+        // GET: Sign
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        public ActionResult Register()
+        {
+            return View();
+        }
+
+        public ActionResult ForgotPassword()
+        {
+            return View();
+        }
+    }
+}
