d6bc7480ed37b9343e813282511ad5a305abf9f6
ertugrulungor <ertugrulungor@gmail.com>
2016-12-20 02:12:33 +0300

    Kullanıcı davet etme sayfası eklendi

--- YPYA/Controllers/ProjectController.cs
+++ YPYA/Controllers/ProjectController.cs
@@ -21,6 +21,9 @@ namespace YPYA.Controllers
 
                 if (id != null)
                 {
+                    Proje p = db.Projes.FirstOrDefault(x => x.Id == id);
+                    ViewBag.projeId = p.Id;
+                    ViewBag.projeAdi = p.Baslik;
                     return View(db.Projes.FirstOrDefault(x => x.Id == id));
                 }
                 else
@@ -40,7 +43,31 @@ namespace YPYA.Controllers
 
                 if (id != null)
                 {
+                    Proje p = db.Projes.FirstOrDefault(x=>x.Id == id);
+                    ViewBag.projeId = p.Id;
+                    ViewBag.projeAdi = p.Baslik;
                     return View(db.Surecs.Where(x=>x.ProjeId == id && x.Kullanici.Id == kulId));
+                }
+                else
+                    return RedirectToAction("Index", "Home");
+            }
+            else return RedirectToAction("Login", "Sign");
+        }
+
+        public ActionResult InvitePeople(int? id)
+        {
+            Session["id"] = 1;
+            if (Session["id"] != null)
+            {
+                int kulId = Convert.ToInt32(Session["id"]);
+                ViewBag.k = db.Kullanicis.FirstOrDefault(x => x.Id == kulId);
+
+                if (id != null)
+                {
+                    Proje p = db.Projes.FirstOrDefault(x => x.Id == id);
+                    ViewBag.projeId = p.Id;
+                    ViewBag.projeAdi = p.Baslik;
+                    return View(db.ProjeKullanicis.Where(x=>x.ProjeId == id));
                 }
                 else
                     return RedirectToAction("Index", "Home");
@@ -52,6 +79,60 @@ namespace YPYA.Controllers
         {
             ViewBag.menuKisi = db.Projes.FirstOrDefault(x => x.Id == id);
             return PartialView();
+        }
+
+        public JsonResult SearchPeople(string content)
+        {
+            List<Object> JsonList = new List<Object>();
+            foreach (Kullanici k in db.Kullanicis.Where(x=>x.KullaniciAdi.Contains(content) || x.Adsoyad.Contains(content)))
+            {
+                var jsonModel = new
+                {
+                    kulId = k.Id,
+                    kulAdi = k.KullaniciAdi,
+                    kulAdSoyad = k.Adsoyad
+                };
+                JsonList.Add(jsonModel);
+            }
+            return Json(JsonList);
+        }
+
+        public JsonResult AddPeopleProject(int id,int projeId)
+        {
+            if (!db.ProjeKullanicis.Any(x => x.KullaniciId == id && x.ProjeId == projeId))
+            {
+                ProjeKullanici pk = new ProjeKullanici()
+                {
+                    KullaniciId = id,
+                    ProjeId = projeId,
+                    Durum = false,
+                    Tarih = DateTime.Now
+                };
+
+                db.ProjeKullanicis.Add(pk);
+                db.SaveChanges();
+
+                Kullanici k = db.Kullanicis.Find(id);
+
+                var jsonModel = new
+                {
+                    durum = 1,
+                    kulAdSoyad = k.Adsoyad,
+                    kulAdi = k.KullaniciAdi,
+                    kulMail = k.Mail
+                };
+
+                return Json(jsonModel, JsonRequestBehavior.AllowGet);
+            }
+            else
+            {
+                var jsonModel = new
+                {
+                    durum = 0
+                };
+
+                return Json(jsonModel, JsonRequestBehavior.AllowGet);
+            }
         }
     }
 }
